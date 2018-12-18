dda9c1f8ccd1963b582c71d5f3b41cc85281ea08
Fayaah <fayah@PC-FAYAH.localdomain>
2018-12-18 17:56:25 +0100

    Fonctionnel

--- cleaner.sh
+++ cleaner.sh
@@ -1,24 +1,35 @@
 #!/bin/bash 
 CURRENT_DIRECTORY=`dirname $0`
-BAZAR_DIRECTORY=$CURRENT_DIRECTORY/../Bazar
-CLEAN_DIRECTORY=$CURRENT_DIRECTORY/../Bazar_Clean 
+BAZAR_DIRECTORY=$CURRENT_DIRECTORY/Bazar
+CLEAN_DIRECTORY=$CURRENT_DIRECTORY/Bazar_Clean 
 
 mkdir Bazar_Clean Bazar_Clean/Divers Bazar_Clean/Images Bazar_Clean/Videos
-FILES=find Bazar -type f 
+
+FILES=`find $BAZAR_DIRECTORY -type f`
 
 for FILE in $FILES 
 do 
-	if [[ `cat $FILE | grep .jpg` ]]
+	if [[ `file $FILE | grep "image data"` ]]
 	then 
-		mv $FILE $CLEAN_DIRECTORY/Images
-	elif [[ `cat $FILE | grep .mp4` ]]
+		echo Envoyé vers image
+		YEAR=`stat -c %y $FILE | cut -c 1-4`
+		MONTH=`stat -c %y $FILE | cut -c 6-7`
+		echo fichier de $MONTH $YEAR
+		mkdir -p $CLEAN_DIRECTORY/Images/$YEAR/$MONTH
+		cp $FILE $CLEAN_DIRECTORY/Images/$YEAR/$MONTH
+
+	elif [[ `file $FILE | grep "ISO Media"` ]]
 	then 
-		mv $FILE $CLEAN_DIRECTORY/Videos
+		echo Envoyé vers vidéo
+		echo `stat -c %y $FILE`
+		cp $FILE $CLEAN_DIRECTORY/Videos
 	else 
-		mv $FILE $CLEAN_DIRECTORY/Divers 	
+		echo Envoyé vers divers
+		echo `stat -c %y $FILE`
+		cp $FILE $CLEAN_DIRECTORY/Divers 	
 	fi
 done
 
-
-#find $directory_name -name \*.jpg
+# find $directory_name -name \*.jpg
+# stat -c %y fichier
 
